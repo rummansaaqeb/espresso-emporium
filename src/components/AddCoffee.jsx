536900f67a104b187f9e5c0ee11821693be952c3
rummansaaqeb <rsaaqeb@gmail.com>
2024-12-04 22:54:41 +0600

    Basic AddCoffee Form Added

--- src/components/AddCoffee.jsx
+++ src/components/AddCoffee.jsx
@@ -2,7 +2,31 @@
 const AddCoffee = () => {
     return (
         <div>
-            <h2>Add A Coffee</h2>               
+            <h2>Add A Coffee</h2>
+            <form>
+                <div className="flex">
+                    <div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Coffee Name</span>
+                            </label>
+                            <label className="input-group">
+                                <input type="text" name="" id="" placeholder="Coffee Name" className="input input-bordered" />
+                            </label>
+                        </div>
+                    </div>
+                    <div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Available Quantity</span>
+                            </label>
+                            <label className="input-group">
+                                <input type="text" name="" id="" placeholder="Available Quantity" className="input input-bordered" />
+                            </label>
+                        </div>
+                    </div>
+                </div>
+            </form>
         </div>
     );
 };
