914b04f5059bc9e105ff30d1143a888336e1d22d
rummansaaqeb <rsaaqeb@gmail.com>
2024-12-04 23:28:29 +0600

    fetched data in mongodb using post method

--- src/components/AddCoffee.jsx
+++ src/components/AddCoffee.jsx
@@ -1,3 +1,4 @@
+import Swal from 'sweetalert2';
 
 const AddCoffee = () => {
 
@@ -16,7 +17,28 @@ const AddCoffee = () => {
         const newCoffee = {
             name, quantity, supplier, taste, category, details, photo
         }
-        console.log(newCoffee)
+        console.log(newCoffee);
+
+        // send data to the server
+        fetch('http://localhost:5000/coffee', {
+            method: "POST",
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newCoffee)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.insertedId) {
+                    Swal.fire({
+                        title: 'Success!',
+                        text: 'Coffee Added Successfully',
+                        icon: 'success',
+                        confirmButtonText: 'Ok'
+                    })
+                }
+            })
     }
 
     return (
